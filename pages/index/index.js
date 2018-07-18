13b5a0d1a01db12d0a0a079fd1b469591b3505a2
zzdylan <527844046@qq.com>
2018-07-18 17:44:37 +0800

    bar

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,9 +5,8 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000
+    bar:{
+      'text':'公告'
+    }
   }
 })
